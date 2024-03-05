2782b2ac31cc3f54c5c82601afaba5d120024f56
scenaristeur <scenaristeur@gmail.com>
2024-03-06 00:28:22 +0100

    default dark

---
+++ website/docs/usage.md
@@ -0,0 +1,9 @@
+# Usage
+
+You can use Igora protocol : 
+- naturally :
+    - igora_web frontend
+- using openai compatible server
+    - obsidian
+    - Microsoft Autogen
+    - CrewAi
--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -67,6 +67,11 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
       navbar: {
         title: 'Igora Protocol',
         logo: {
