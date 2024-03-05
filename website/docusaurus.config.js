30d9ab560aa1e29e8ab161c122617f53be455fc8
scenaristeur <scenaristeur@gmail.com>
2024-03-06 00:28:53 +0100

    default dark

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -72,6 +72,14 @@ const config = {
         disableSwitch: false,
         respectPrefersColorScheme: false,
       },
+      announcementBar: {
+        id: 'support_us',
+        content:
+          'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
+        backgroundColor: '#fafbfc',
+        textColor: '#091E42',
+        isCloseable: false,
+      },
       navbar: {
         title: 'Igora Protocol',
         logo: {
