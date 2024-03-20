a76fe4b8fdbc8dffdf13b002f875f3b42ac3a3c5
scenaristeur <scenaristeur@gmail.com>
2024-03-20 20:44:33 +0100

    systemprompt dialog

--- clients/libreoffice/24.2.1/llm/macros/say_hello.py
+++ clients/libreoffice/24.2.1/llm/macros/say_hello.py
@@ -2,52 +2,87 @@ from __future__ import annotations
 from ooodev.loader import Lo
 from ooodev.write import WriteDoc
 from ooodev.dialog.input import Input
-from ooodev.dialog import Dialog # https://python-ooo-dev-tools.readthedocs.io/en/main/src/dialog/dialog.html
-from my_mod.radio_group_box import RadioGroupBox
+# https://python-ooo-dev-tools.readthedocs.io/en/main/src/dialog/dialog.html
+from ooodev.dialog import Dialog
+from my_mod.llm_config_d import LlmConfigD
 
+
+
+config = {"actions": [
+    {"name": "Etendre", "systemPrompt": "Agis comme un écrivain. Développe le texte suivant en ajoutant plus de détails, tout en gardant le même sens. Sors uniquement le texte et rien d'autre. Ne discute pas, pas de préambule, va à l'essentiel."},
+    {"name": "Résumer", "systemPrompt": "Tu es un écrivain"}
+    ]}
 
 
 def say_hello(*args):
     doc = Lo.current_doc
     doc.msgbox("Hello, world!")
 
-def ecrire_salut():
-    doc = WriteDoc.from_current_doc()    
+
+def ecrire_salut(arg1 = None):
+    print("wwhat is arg1",arg1)
+    doc = WriteDoc.from_current_doc()
     cursor = doc.get_cursor()
     cursor.append_para(text="Salut")
 
 
 def input_system_prompt():
     ps = Input.get_input(title="System Prompt", msg="System prompt")
-    doc = WriteDoc.from_current_doc()    
+    doc = WriteDoc.from_current_doc()
     cursor = doc.get_cursor()
     cursor.append_para(text=ps)
 
+
 def config_dialog():
-    #https://python-ooo-dev-tools.readthedocs.io/en/main/src/dialog/dialog.html
+    # https://python-ooo-dev-tools.readthedocs.io/en/main/src/dialog/dialog.html
     dialog = Dialog(title="Config", x=100, y=100, width=300, height=400)
-    #dialog.insert_button("OK", 10,10,60, height=20, btn_type=None, name='valid')
-    dialog.insert_text_field(text="Text", x=10,y=10,width=60, height=20)
+    # dialog.insert_button("OK", 10,10,60, height=20, btn_type=None, name='valid')
+    dialog.insert_text_field(text="Text", x=10, y=10, width=60, height=20)
 
-
-
-
-    ok_btn=dialog.insert_button(label="OK", x=10,y=300, width=50, name='ok')
+    ok_btn = dialog.insert_button(label="OK", x=10, y=300, width=50, name='ok')
     ok_btn.add_event_mouse_released(lambda event: ecrire_salut())
-    #dialog.execute() # https://python-ooo-dev-tools.readthedocs.io/en/main/src/dialog/dialog.html#ooodev.dialog.Dialog.execute
+    # dialog.execute() # https://python-ooo-dev-tools.readthedocs.io/en/main/src/dialog/dialog.html#ooodev.dialog.Dialog.execute
     dialog.set_visible(True)
     dialog.execute()
     dialog.dispose()
     dialog.set_visible(False)
-    #dialog.to_front()
+    # dialog.to_front()
 
-def radio():
-    rg = RadioGroupBox() # TODO : import radio_group_box.py
-    rg.show()
+
+def llm_config(arg1 = None):
+    print("wwhat is arg1 with icon in toolbar",arg1)
+    dialog = LlmConfigD("Fruits", "JetBrains Mono NL", config)
+    state = dialog.get_text()
+    # llmcd = LlmConfigDialog() # TODO : import radio_group_box.py
+    # state =llmcd.getState()
+    # #llmcd.show()
+    # # # llmcd.set_truc("machin")
+    # # state =llmcd.get_truc()
+
+    # #llmcd.dispose()
+
+    doc = WriteDoc.from_current_doc()
+    cursor = doc.get_cursor()
+    #cursor.append_para(text=state)
+    #dialog.set_text("Vegetables")
+    #state = dialog.get_text()
+    #cursor.append_para(text=state)
+    #dialog.set_config(config)
+    #state = dialog.get_config()
+    #cursor.append_para(text=state["actions"][0]["systemPrompt"])
+
+
+    dialog.show()
+    state = dialog.get_text()
+    cursor.append_para(text=state)
+    dialog.dispose()
+
+
+
 
 
 def print_config_DOESNOTWORK():
-    doc = WriteDoc.from_current_doc()    
+    doc = WriteDoc.from_current_doc()
     cursor = doc.get_cursor()
     cursor.append_para(text="Salut")
     with Lo.Loader(Lo.ConnectSocket(headless=True)) as loader:
@@ -57,10 +92,12 @@ def print_config_DOESNOTWORK():
         print(f"OS Architecture: {platform.architecture()}")
 
         print(f"\nOffice Name: {Info.get_config('ooName')}")
-        print(f"\nOffice version (long): {Info.get_config('ooSetupVersionAboutBox')}")
+        print(
+            f"\nOffice version (long): {Info.get_config('ooSetupVersionAboutBox')}")
         print(f"Office version (short): {Info.get_config('ooSetupVersion')}")
         print(f"\nOffice language location: {Info.get_config('ooLocale')}")
-        print(f"System language location: {Info.get_config('ooSetupSystemLocale')}")
+        print(
+            f"System language location: {Info.get_config('ooSetupSystemLocale')}")
 
         print(f"\nWorking Dir: {Info.get_paths('Work')}")
         print(f"\nOffice Dir: {Info.get_office_dir()}")
@@ -69,4 +106,6 @@ def print_config_DOESNOTWORK():
         print(f"\nTemplates Dirs: {Info.get_paths('Template')}")
         print(f"\nGallery Dir: {Info.get_paths('Gallery')}")
 
-g_exportedScripts = (say_hello, ecrire_salut, input_system_prompt, config_dialog, radio)
+
+g_exportedScripts = (say_hello, ecrire_salut,
+                     input_system_prompt, config_dialog, llm_config)
---
+++ clients/libreoffice/24.2.1/llm/my_mod/llm_config_d.py
@@ -0,0 +1,393 @@
+from __future__ import annotations
+import uno
+from typing import Any, cast, TYPE_CHECKING
+from ooo.dyn.awt.push_button_type import PushButtonType
+from ooo.dyn.awt.pos_size import PosSize
+from ooo.dyn.awt.font_descriptor import FontDescriptor
+from ooodev.dialog.msgbox import MsgBox, MessageBoxResultsEnum, MessageBoxType
+
+from ooodev.dialog import Dialogs, BorderKind
+from ooodev.events.args.event_args import EventArgs
+from ooodev.loader import Lo
+from ooodev.utils.info import Info
+from ooodev.utils.color import StandardColor
+
+if TYPE_CHECKING:
+    from com.sun.star.awt import ActionEvent
+    from com.sun.star.awt import ItemEvent
+    from ooodev.dialog.dl_control.ctl_radio_button import CtlRadioButton
+    from ooodev.dialog.dl_control.ctl_input_field import CtlInputField
+
+
+class LlmConfigD:
+    def __init__(self, text, font, config):
+        self._text = text
+        self._font = font
+        self._config = config
+        self._border_kind = BorderKind.BORDER_SIMPLE
+        self._width = 400
+        self._height = 310
+        self._btn_width = 100
+        self._btn_height = 30
+        self._margin = 6
+        self._box_height = 30
+        self._title = "Llm Config"
+        self._msg = "config your llm settings"
+        if self._border_kind != BorderKind.BORDER_3D:
+            self._padding = 10
+        else:
+            self._padding = 14
+        self._current_tab_index = 1
+        self._group1_opt: CtlRadioButton | None = None
+        self._group2_opt: CtlRadioButton | None = None
+        self._system_prompt: CtlInputField | None = None
+        # get or set a font descriptor. This helps to keep the font consistent across different platforms.
+        fd = Info.get_font_descriptor("Liberation Serif", "Regular")
+        if fd is None:
+            fd = FontDescriptor(
+                CharacterWidth=100.0,
+                Kerning=True,
+                WordLineMode=False,
+                Pitch=2,
+                Weight=100,
+            )
+        fd.Height = 10
+        self._fd = fd
+        self._init_dialog()
+
+    def get_config(self):
+        return self._config
+
+    def set_config(self, value):
+        self._config = value
+
+    def get_text(self):
+        return self._text
+
+    def set_text(self, value):
+        self._text = value
+
+    def get_font(self):
+        return self._font
+
+    def set_font(self, value):
+        self._font = value
+
+    def _init_dialog(self) -> None:
+        """Create dialog and add controls."""
+        self._init_handlers()
+        self._dialog = Dialogs.create_dialog(
+            x=-1, y=-1, width=self._width, height=self._height, title=self._title
+        )
+        self._dialog.set_visible(False)
+        Dialogs.create_dialog_peer(self._dialog.control)
+        self._init_label()
+        self._init_system_prompt()
+        # self._init_group_boxes()
+        # self._init_radio_controls()
+        self._init_buttons()
+
+    def _init_handlers(self) -> None:
+        """
+        Add event handlers for when changes occur.
+
+        Methods can not be assigned directly to control callbacks.
+        This is a python thing. However, methods can be assigned to class
+        variable an in turn those can be assigned to callbacks.
+
+        Example:
+            ``self._ctl_btn_info.add_event_action_performed(self.on_button_action_preformed)``
+            This would not work!
+
+            ``self._ctl_btn_info.add_event_action_performed(self._fn_button_action_preformed)``
+            This will work.
+        """
+
+        # self._fn_on_group1_changed = self.on_group1_changed
+        # self._fn_on_group2_changed = self.on_group2_changed
+        self._fn_button_action_preformed = self.on_button_action_preformed
+        self._fn_on_system_prompt_changed = self.on_system_prompt_changed
+
+    def _init_label(self) -> None:
+        """Add a fixed text label to the dialog control"""
+        self._ctl_main_lbl = Dialogs.insert_label(
+            dialog_ctrl=self._dialog.control,
+            label=self._msg,
+            x=self._margin,
+            y=self._padding,
+            width=self._width - (self._padding * 2),
+            height=self._box_height,
+        )
+        self._ctl_main_lbl.set_font_descriptor(self._fd)
+        self._ctl_main_lbl.font_descriptor.weight = 150  # make bold
+
+    def _init_system_prompt(self) -> None:
+        """Add a text field to the dialog control"""
+        sz_lbl = self._ctl_main_lbl.view.getPosSize()
+        self._ctl_sys_prompt = Dialogs.insert_text_field(
+            dialog_ctrl=self._dialog.control,
+            x=self._margin,
+            y=self._padding + sz_lbl.X + sz_lbl.Height,
+            height=self._height
+            - (sz_lbl.X + sz_lbl.Height)
+            - self._btn_height
+            - (self._padding * 3),
+            width=round(self._width / 2) - (self._margin * 2),
+            text=self._config["actions"][0]["systemPrompt"],
+        )
+
+    # def _init_group_boxes(self) -> None:
+    #     sz_lbl = self._ctl_main_lbl.view.getPosSize()
+    #     self._ctl_gb1 = Dialogs.insert_group_box(
+    #         dialog_ctrl=self._dialog.control,
+    #         x=self._margin,
+    #         y=self._padding + sz_lbl.X + sz_lbl.Height,
+    #         height=self._height
+    #         - (sz_lbl.X + sz_lbl.Height)
+    #         - self._btn_height
+    #         - (self._padding * 3),
+    #         width=round(self._width / 2) - (self._margin * 2),
+    #         label="Action Selection",
+    #     )
+    #     self._ctl_gb1.set_font_descriptor(self._fd)
+    #     self._ctl_gb1.font_descriptor.weight = 150  # make bold
+
+    #     sz = self._ctl_gb1.view.getPosSize()
+    #     self._ctl_gb2 = Dialogs.insert_group_box(
+    #         dialog_ctrl=self._dialog.control,
+    #         x=sz.X + sz.Width + (self._margin * 2),
+    #         y=sz.Y,
+    #         height=sz.Height,
+    #         width=sz.Width,
+    #         label="Group Box TWO",
+    #     )
+    #     self._ctl_gb2.set_font_descriptor(
+    #         self._ctl_gb1.font_descriptor.component)
+
+    def _init_buttons(self) -> None:
+        """Add OK, Cancel and Info buttons to dialog control"""
+        self._ctl_btn_cancel = Dialogs.insert_button(
+            dialog_ctrl=self._dialog.control,
+            label="Cancel",
+            x=self._width - self._btn_width - self._margin,
+            y=self._height - self._btn_height - self._padding,
+            width=self._btn_width,
+            height=self._btn_height,
+            btn_type=PushButtonType.CANCEL,
+        )
+        self._ctl_btn_cancel.set_font_descriptor(self._fd)
+        self._ctl_btn_cancel.text_color = StandardColor.BLACK
+        self._ctl_btn_cancel.background_color = StandardColor.RED_LIGHT1
+
+        sz = self._ctl_btn_cancel.view.getPosSize()
+        self._ctl_btn_ok = Dialogs.insert_button(
+            dialog_ctrl=self._dialog.control,
+            label="OK",
+            x=sz.X - sz.Width - self._margin,
+            y=sz.Y,
+            width=self._btn_width,
+            height=self._btn_height,
+            btn_type=PushButtonType.OK,
+            DefaultButton=True,
+        )
+        self._ctl_btn_ok.set_font_descriptor(self._fd)
+        self._ctl_btn_ok.text_color = StandardColor.BLACK
+        self._ctl_btn_ok.background_color = StandardColor.GREEN_LIGHT1
+
+        self._ctl_btn_info = Dialogs.insert_button(
+            dialog_ctrl=self._dialog.control,
+            label="Info",
+            x=self._margin,
+            y=sz.Y,
+            width=self._btn_width,
+            height=self._btn_height,
+        )
+        self._ctl_btn_info.set_font_descriptor(self._fd)
+        self._ctl_btn_info.view.setActionCommand("INFO")
+        self._ctl_btn_info.model.HelpText = "Show info for selected items."
+        self._ctl_btn_info.add_event_action_performed(
+            self._fn_button_action_preformed)
+
+    # def _init_radio_controls(self) -> None:
+    #     """
+    #     Inserts radio buttons into dialog.
+
+    #     Inserting more then a single group of radio buttons into a dialog can be a bit buggy.
+    #     This in part has to do with how LibreOffice handles the Tab Indexes for the radio controls.
+
+    #     The easies solution is to set the group box tab index right before adding a set of radio controls.
+    #     This way between radio control set a different control is assigned a tab index before the next
+    #     set of radio controls is created and added.
+
+    #     When using tab indexes the tab indexes must be contiguous for each group but the next group must not
+    #     pick up from the same tab index that the last group finished on.
+
+    #     Alternatively it seems radio control groups can also be added with out setting a tab index as well.
+    #     In order for this to work a different control such as a group box must be added to the dialog between
+    #     adding radio control groups.
+    #     """
+
+    #     sz_gb1 = self._ctl_gb1.view.getPosSize()
+    #     self._rb1 = Dialogs.insert_radio_button(
+    #         dialog_ctrl=self._dialog.control,
+    #         label="Group 1 - Radio Button 1",
+    #         x=sz_gb1.X + self._margin,
+    #         y=sz_gb1.Y + self._box_height,
+    #         width=sz_gb1.Width - (self._margin * 2),
+    #         height=20,
+    #     )
+    #     self._rb1.set_font_descriptor(self._fd)
+    #     self._rb1.text_color = StandardColor.get_random_color()
+    #     self._group1_opt = self._rb1
+    #     self._ctl_gb1.model.TabIndex = self._current_tab_index
+    #     self._current_tab_index += 1
+
+    #     self._rb1.model.State = 1
+    #     self._rb1.tab_index = self._current_tab_index
+    #     self._current_tab_index += 1
+    #     extra = 8
+    #     self._rb1.add_event_item_state_changed(self._fn_on_group1_changed)
+    #     rb1_sz = self._rb1.view.getPosSize()
+    #     for i in range(1, extra + 1):
+    #         radio_btn = Dialogs.insert_radio_button(
+    #             dialog_ctrl=self._dialog.control,
+    #             label=f"Group 1 - Radio Button {i + 1}",
+    #             x=rb1_sz.X,
+    #             y=rb1_sz.Y + (rb1_sz.Height * i),
+    #             width=rb1_sz.Width,
+    #             height=rb1_sz.Height,
+    #         )
+    #         radio_btn.set_font_descriptor(self._rb1.font_descriptor.component)
+    #         radio_btn.text_color = StandardColor.get_random_color()
+    #         radio_btn.tab_index = self._current_tab_index
+    #         radio_btn.model.State = 0
+    #         self._current_tab_index += 1
+    #         radio_btn.add_event_item_state_changed(self._fn_on_group1_changed)
+
+    #     # simplest way to break contiguous tab order and have it work
+    #     self._ctl_gb2.model.TabIndex = self._current_tab_index
+    #     self._current_tab_index += 1
+
+    #     sz_gb2 = self._ctl_gb2.view.getPosSize()
+    #     self._rb2 = Dialogs.insert_radio_button(
+    #         dialog_ctrl=self._dialog.control,
+    #         label="Group 2 - Radio Button 1",
+    #         x=sz_gb2.X + self._margin,
+    #         y=sz_gb2.Y + self._box_height,
+    #         width=sz_gb2.Width - (self._margin * 2),
+    #         height=rb1_sz.Height,
+    #         # name="Radio1",
+    #     )
+    #     self._rb2.set_font_descriptor(self._rb1.font_descriptor.component)
+    #     self._rb2.text_color = StandardColor.get_random_color()
+    #     self._rb2.model.State = 1
+    #     self._rb2.tab_index = self._current_tab_index
+    #     self._group2_opt = self._rb2
+    #     self._current_tab_index += 1
+    #     self._rb2.add_event_item_state_changed(self._fn_on_group2_changed)
+    #     rb2_sz = self._rb2.view.getPosSize()
+    #     for action in self._config["actions"]:
+    #         radio_btn = Dialogs.insert_radio_button(
+    #             dialog_ctrl=self._dialog.control,
+    #             label=f" {action}",
+    #             x=rb2_sz.X,
+    #             y=rb2_sz.Y + (rb2_sz.Height * i),
+    #             width=rb2_sz.Width,
+    #             height=rb2_sz.Height,
+    #             # name=f"Radio{i + 1}",
+    #         )
+    #         radio_btn.set_font_descriptor(self._rb1.font_descriptor.component)
+    #         radio_btn.text_color = StandardColor.get_random_color()
+    #         radio_btn.model.State = 0
+    #         radio_btn.tab_index = self._current_tab_index
+    #         self._current_tab_index += 1
+    #         radio_btn.add_event_item_state_changed(self._fn_on_group2_changed)
+
+    #     self._current_tab_index += 1
+
+    # endregion Init
+
+    # region Handle Results
+    def _handle_results(self, result: int) -> None:
+        """Display a message if the OK button has been clicked"""
+        if result == MessageBoxResultsEnum.OK.value:
+            #msg = ""
+            # msg = str(self._ctl_sys_prompt)
+            msg = self._ctl_sys_prompt.text
+            # if self._system_prompt:
+            #     msg += f"System Prompt: {self._system_prompt}\n"
+
+            # if self._group1_opt:
+            #     msg += f"Selected Group 1: {self._group1_opt.model.Label}"
+            # if self._group2_opt:
+            #     if msg:
+            #         msg += f"\nSelected Group 2: {self._group2_opt.model.Label}"
+            #     else:
+            #         msg += f"Selected Group 2: {self._group2_opt.model.Label}"
+            # if msg == "":
+            #     # should not happen
+            #     msg = "No Options are selected"
+            self.set_text(msg)
+            # _ = MsgBox.msgbox(
+            #     msg=msg,
+            #     title="Selected Values",
+            #     boxtype=MessageBoxType.INFOBOX,
+            # )
+
+    # endregion Handle Results
+
+    # region Event Handlers
+    def on_system_prompt_changed(
+        self, src: Any, event: EventArgs, control_src: CtlInputField, *args, **kwargs
+    )   -> None:
+        self._system_prompt = control_src.text
+        prompt = str(control_src.text)
+        print(prompt)
+
+
+
+    # def on_group1_changed(
+    #     self, src: Any, event: EventArgs, control_src: CtlRadioButton, *args, **kwargs
+    # ) -> None:
+    #     itm_event = cast("ItemEvent", event.event_data)
+    #     print("Group 1 Item ID:", itm_event.ItemId)
+    #     print("Group 1 Tab Index:", control_src.tab_index)
+    #     print("Group 1 Tab Name:", control_src.model.Name)
+    #     self._group1_opt = control_src
+
+    # def on_group2_changed(
+    #     self, src: Any, event: EventArgs, control_src: CtlRadioButton, *args, **kwargs
+    # ) -> None:
+    #     itm_event = cast("ItemEvent", event.event_data)
+    #     print("Group 2 Item ID:", itm_event.ItemId)
+    #     print("Group 2 Tab Index:", control_src.tab_index)
+    #     print("Group 2 Tab Name:", control_src.model.Name)
+    #     self._group2_opt = control_src
+
+    def on_button_action_preformed(
+        self, src: Any, event: EventArgs, control_src: Any, *args, **kwargs
+    ) -> None:
+        """Method that is fired when Info button is clicked."""
+        itm_event = cast("ActionEvent", event.event_data)
+        if itm_event.ActionCommand == "INFO":
+            self._handle_results(MessageBoxResultsEnum.OK.value)
+
+    # endregion Event Handlers
+
+    # region Show Dialog
+    def show(self) -> int:
+        window = Lo.get_frame().getContainerWindow()
+        ps = window.getPosSize()
+        x = round(ps.Width / 2 - self._width / 2)
+        y = round(ps.Height / 2 - self._height / 2)
+        self._dialog.set_pos_size(
+            x, y, self._width, self._height, PosSize.POSSIZE)
+        self._dialog.set_visible(True)
+        result = self._dialog.execute()
+        self._handle_results(result)
+
+        return result
+
+    # endregion Show Dialog
+    def dispose(self):
+        self._dialog.dispose()
