bf5105f132866510f2f30274c8612a6659d3dfc9
scenaristeur <scenaristeur@gmail.com>
2024-03-31 01:16:44 +0100

    openai

--- openai/openai.js
+++ openai/openai.js
@@ -84,7 +84,6 @@ app.get("/v1/models", (req, res) => {
 app.post("/v1/chat/completions", express.json(), async (req, res) => {
   console.log("received", req.body);
 
-
   // CREATE RESPONSE
   let chatCompletionReponse = new ChatCompletionResponse(req.body);
   console.log("chatCompletionReponse", chatCompletionReponse);
@@ -106,297 +105,89 @@ app.post("/v1/chat/completions", express.json(), async (req, res) => {
     temperature: req.body.temperature || 0,
     date: Date.now(),
   };
-// CREATE LISTENERS
-if (req.body.stream == true) {
-  res.writeHead(200, {
-    "Cache-Control": "no-store",
-    // "Content-Type": "text/plain",
-    "Content-Type": "text/event-stream",
-    "Transfer-Encoding": "chunked",
-  });
-
-doing.observeDeep((events, transaction) => {
-  let current_doing = doing.get(chatCompletionReponse.id);
-  if (current_doing != undefined && current_doing.delta != undefined && current_doing.delta.content != undefined && current_doing.chunkDate != chatCompletionReponse.chunkDate) {
-    console.log("\n CURENT DOING ", current_doing);
-    chatCompletionReponse.updateContent(current_doing.delta.content, current_doing.chunkDate);
-    res.write("data: " + chatCompletionReponse.toString() + "\n\n");
-  }
-})
-done.observeDeep((events, transaction) => {
-  let current_done = done.get(chatCompletionReponse.id);
-  if (current_done != undefined){
-    console.log("\n CURRENT done", current_done);
-   
-    chatCompletionReponse.finish("stop");
-    // Done ne fonctionne pas avec autogen
-      // res.write("data: " + chatCompletionReponse.toString() + "\n\n[DONE]\n\n");
-      res.write("data: " + chatCompletionReponse.toString());
-      res.end();
-     // console.log("FINISH", chatCompletionReponse.toString() )
-  }
-})
-
-}else {
-  let response = {
-    id: todo.id,
-    choices: [
-      {
-        message: {
-          content: "Hello, World!",
-          role: "assistant",
-          function_call: null, // "None" ? python style ?
-          tool_calls: null,
-        },
-        finish_reason: "stop",
-        index: 0,
-        logprobs: null,
-        model: model, //"gpt-3.5-turbo", //"vicuna-7b-v1.5-16k.Q2_K.gguf"
-        created: Date.now() / 1000, //  1703395008
-        object: "chat.completion",
-        system_fingerprint: "None",
-        usage: { completion_tokens: 299, prompt_tokens: 36, total_tokens: 335 },
-      },
-    ],
-  };
-
-  console.log("streaming is false ", todo)
-
-  console.log(todo);
-  todos.set(todo.id, todo);
-
-
-  let textPromise = new Promise((resolve, reject) => {
-    let timer = setInterval(async function () {
-      let check = done.get(todo.id);
-      if (check != undefined) {
-        console.log("PROMISE DONE", check);
-        // result.end = Date.now();
-        // console.log("--GENERATION\n", check.data.generations[0], "\n--");
-        // let text =
-        //   check.data.generations[0] && check.data.generations[0].text.trim();
-
-        // console.log("----- text generated : ", text, "\n-----\n");
-
-        // result.job = check.data.generations[0];
-
-        clearInterval(timer); // Stop the timer
-        resolve(check); // Résoudre la promesse avec le texte
-      } 
-    }, 1000);
-  });
-
-  const result = await textPromise; // Attendre que la promesse soit résolue
-  //result.text = text;
-  //console.log("RETURN RESULT", result);
-  response.choices[0].message.content = result.response.trim();
-  //   stream.write(JSON.stringify(result) + "\r\n");
-  console.log("response", response);
-  res.status(200).json(response);
-}
-
-
-
-
-
-});
-
-app.post("/v1/chat/completions1", express.json(), async (req, res) => {
-  console.log("received", req.body);
-
-  let id = "chatcmpl-" + uuidv4();
-  let model = req.body.model;
-
-  let todo = {
-    //id: req.body.id, //id,
-    id: id,
-    asker: openai_server_id, // should be client id
-    type: "text",
-    // systemPrompt should comme from client
-    systemPrompt: `Tu es un assistant chargé de répondre au mieux à la demande de l'utilisateur`,
-    //prompt:
-    messages: req.body.messages,
-    state: "todo",
-    model: model,
-    // seed: options.seed || 0,
-    temperature: req.body.temperature || 0,
-    date: Date.now(),
-  };
-  console.log(todo);
-  todos.set(todo.id, todo);
-
-  // reponse inspirée de https://cookbook.openai.com/examples/how_to_stream_completions
-  let response = {
-    id: id,
-    choices: [
-      {
-        message: {
-          content: "Hello, World!",
-          role: "assistant",
-          function_call: null, // "None" ? python style ?
-          tool_calls: null,
-        },
-        finish_reason: "stop",
-        index: 0,
-        logprobs: null,
-        model: model, //"gpt-3.5-turbo", //"vicuna-7b-v1.5-16k.Q2_K.gguf"
-        created: Date.now() / 1000, //  1703395008
-        object: "chat.completion",
-        system_fingerprint: "None",
-        usage: { completion_tokens: 299, prompt_tokens: 36, total_tokens: 335 },
-      },
-    ],
-  };
-
-  // stream https://github.com/hyperonym/basaran/blob/a73dc6e712a6b5e672fe80cdc12a25451ef2c0ce/basaran/__main__.py#L176
+  // CREATE LISTENERS
   if (req.body.stream == true) {
     res.writeHead(200, {
-      "Content-Type": "text/plain",
+      "Cache-Control": "no-store",
+      // "Content-Type": "text/plain",
+      "Content-Type": "text/event-stream",
       "Transfer-Encoding": "chunked",
     });
 
-    // FIRST
-    res.write(
-      JSON.stringify({
-        id: id,
-        object: "chat.completion.chunk",
-        created: Date.now() / 1000,
-        model: model,
-        system_fingerprint: "fp_44709d6fcb",
-        choices: [
-          {
-            index: 0,
-            delta: { role: "assistant", content: "" },
-            logprobs: null,
-            finish_reason: null,
-          },
-        ],
-      })
-    );
-
     doing.observeDeep((events, transaction) => {
-      //this.log("events", events, transaction)
-      // this.prepare();
-      let chunky = doing.get(id);
-      console.log("chunky", chunky);
-
-      res.write(
-        JSON.stringify({
-          id: id,
-          object: "chat.completion.chunk",
-          created: Date.now() / 1000,
-          model: model,
-          system_fingerprint: "fp_44709d6fcb",
-          choices: [
-            {
-              index: 0,
-              delta: chunky.delta,
-              logprobs: null,
-              finish_reason: null,
-            },
-          ],
-        })
-      ) + "\n";
+      let current_doing = doing.get(chatCompletionReponse.id);
+      if (
+        current_doing != undefined &&
+        current_doing.delta != undefined &&
+        current_doing.delta.content != undefined &&
+        current_doing.chunkDate != chatCompletionReponse.chunkDate
+      ) {
+        console.log("\n CURENT DOING ", current_doing.delta.content);
+        chatCompletionReponse.updateContent(
+          current_doing.delta.content,
+          current_doing.chunkDate
+        );
+        res.write("data: " + chatCompletionReponse.toString() + "\n\n");
+      }
     });
+    done.observeDeep((events, transaction) => {
+      let current_done = done.get(chatCompletionReponse.id);
+      if (current_done != undefined) {
+        console.log("\n CURRENT done", current_done);
 
-    let chunks = 2;
-
-    // if (chunks > 100) chunks = 100;
-
-    var count = 1;
-
-    while (count <= chunks) {
-      // res.write(
-      //   JSON.stringify({
-      //     type: "stream",
-      //     chunk: count++,
-      //   }) + "\n"
-      // );
-
-      // res.write(
-      //   JSON.stringify({
-      //     type: "stream",
-      //     chunk: count++,
-      //   }) + "\n"
-      // );
-
-      // CHUNK
-      count++;
-      res.write(
-        JSON.stringify({
-          id: id,
-          object: "chat.completion.chunk",
-          created: Date.now() / 1000,
-          model: model,
-          system_fingerprint: "fp_44709d6fcb",
-          choices: [
-            {
-              index: 0,
-              delta: { content: "Hello" },
-              logprobs: null,
-              finish_reason: null,
-            },
-          ],
-        })
-      ) + "\n";
-    }
-
-    // END
-
-    res.write(
-      JSON.stringify({
-        id: id,
-        object: "chat.completion.chunk",
-        created: Date.now() / 1000,
-        model: model,
-        system_fingerprint: "fp_44709d6fcb",
-        choices: [
-          { index: 0, delta: {}, logprobs: null, finish_reason: "stop" },
-        ],
-      })
-    );
-
-    res.end();
-    // res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    // res.setHeader('Transfer-Encoding', 'chunked');
-
-    // res.write("Thinking...");
-    // sendAndSleep(res, 1);
-    //   res.writeHead(200, {
-    //     "Content-Type": "text/event-stream",
-    //     "Cache-Control": "no-cache",
-    //     Connection: "keep-alive",
-    //   });
-    //   res.write("data: " + JSON.stringify(response) + "\n\n");
-    //   res.flush();
+        chatCompletionReponse.finish("stop");
+        // Done ne fonctionne pas avec autogen
+        // res.write("data: " + chatCompletionReponse.toString() + "\n\n[DONE]\n\n");
+        res.write("data: " + chatCompletionReponse.toString());
+        res.end();
+        // console.log("FINISH", chatCompletionReponse.toString() )
+      }
+    });
   } else {
+    let response = {
+      id: todo.id,
+      choices: [
+        {
+          message: {
+            content: "Hello, World!",
+            role: "assistant",
+            function_call: null, // "None" ? python style ?
+            tool_calls: null,
+          },
+          finish_reason: "stop",
+          index: 0,
+          logprobs: null,
+          model: model, //"gpt-3.5-turbo", //"vicuna-7b-v1.5-16k.Q2_K.gguf"
+          created: Date.now() / 1000, //  1703395008
+          object: "chat.completion",
+          system_fingerprint: "None",
+          usage: {
+            completion_tokens: 0,
+            prompt_tokens: 0,
+            total_tokens: 0,
+          },
+        },
+      ],
+    };
+
+    // console.log("streaming is false ", todo);
+
+    // console.log(todo);
+    todos.set(todo.id, todo);
+
     let textPromise = new Promise((resolve, reject) => {
       let timer = setInterval(async function () {
         let check = done.get(todo.id);
         if (check != undefined) {
           console.log("PROMISE DONE", check);
-          // result.end = Date.now();
-          // console.log("--GENERATION\n", check.data.generations[0], "\n--");
-          // let text =
-          //   check.data.generations[0] && check.data.generations[0].text.trim();
-
-          // console.log("----- text generated : ", text, "\n-----\n");
-
-          // result.job = check.data.generations[0];
-
           clearInterval(timer); // Stop the timer
           resolve(check); // Résoudre la promesse avec le texte
-        } else {
-          console.log(check);
         }
       }, 1000);
     });
 
     const result = await textPromise; // Attendre que la promesse soit résolue
-    //result.text = text;
-    //console.log("RETURN RESULT", result);
     response.choices[0].message.content = result.response.trim();
-    //   stream.write(JSON.stringify(result) + "\r\n");
     console.log("response", response);
     res.status(200).json(response);
   }
