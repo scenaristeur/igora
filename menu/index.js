2123f12248cb3c13b1ccfeaa2fdc722fa0928805
scenaristeur <scenaristeur@gmail.com>
2024-04-19 12:35:48 +0200

    mermaid start

--- menu/index.js
+++ menu/index.js
@@ -3,15 +3,45 @@ import { input, checkbox, Separator } from '@inquirer/prompts';
 
 
 const services = await checkbox({
-    message: 'Select a package manager',
+    theme: {
+        prefix: `Mise en place de la configuration d'IGORA
+.------.------.    
++-------------+                     ___        |      |      |    
+|             |                     \ /]       |      |      |    
+|             |        _           _(_)        |      |      |    
+|             |     ___))         [  | \___    |      |      |    
+|             |     ) //o          | |     \   |      |      |    
+|             |  _ (_    >         | |      ]  |      |      |    
+|          __ | (O)  \__<          | | ____/   '------'------'    
+|         /  o| [/] /   \)        [__|/_                          
+|             | [\]|  ( \         __/___\_____                    
+|             | [/]|   \ \__  ___|            |                   
+|             | [\]|    \___E/%%/|____________|_____              
+|             | [/]|=====__   (_____________________)             
+|             | [\] \_____ \    |                  |              
+|             | [/========\ |   |                  |              
+|             | [\]     []| |   |                  |              
+|             | [/]     []| |_  |                  |              
+|             | [\]     []|___) |                  |    MEPH          
+===================================================================
+
+
+`
+    },
+    message: 'Selectionnez les services et les modèles que vous souhaitez lancer.\n',
     choices: [
-        new Separator("-- Serveurs d'inférence LLM --"),
+        new Separator("\n\n-- Serveurs d'inférence LLM --"),
         { name: 'node-llama-cpp', value: 's_nlc' },
         { name: 'llama-cpp-python', value: 's_lcp' },
         new Separator("-- Modèles --"),
         { name: 'dolphin', value: 'm_dolphin' },
         { name: 'llama', value: 'm_llama' },
         { name: 'hermes', value: 'm_hermes' },
+        new Separator("-- Autres services et outils --"),
+        { name: 'bigAgi chat interface', value: 't_bigagi' },
+        { name: 'Serveur Compatible OpenAi', value: 't_openai_compatible' },
+        { name: 'Stockage History', value: 't_stockage_history' },
+        { name: 'Authentification', value: 't_auth' },
         // { name: 'pnpm', value: 'pnpm', disabled: true },
         // {
         //     name: 'pnpm',
@@ -26,3 +56,28 @@ console.log(services)
 
 const config_name = await input({ message: 'Nom de cette config' });
 console.log(config_name)
+
+
+launch_services(services)
+
+function launch_services(arr) {
+    arr.forEach(function(value, index) {
+      switch (value) {
+        case 's_nlc':
+          console.log('This is s_nlc lancement node llama cpp');
+          break;
+        case 's_lcp':
+          console.log('Lancement serveur python');
+          break;
+        case 'third':
+          console.log('This is third');
+          break;
+        case 'fourth':
+          console.log('This is fourth');
+          break;
+        default:
+          console.log('None');
+      }
+    });
+  
+  }
